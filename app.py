7854b3b2155c2d9ca944ede604781bf262678ce8
Binh D. Nguyen <stepup.ored@gmail.com>
2023-03-31 01:37:16 +0300

    update app with demo transactions and get owner wallet balance

--- app.py
+++ app.py
@@ -1,29 +1,23 @@
 from blockchain import ORDchain
 from block.hash import hash
+from transaction.transaction import Transaction
+
+# demo transaction (assumming that all transactions are valid)
+transaction_03_31 = Transaction([
+    {"from": "jame", "to": "ored95", "amount": 1000},
+    {"from": "ored95", "to": "alex", "amount": 100},
+    {"from": "ored95", "to": "john wick", "amount": 500}])
+
+transaction_04_01 = Transaction([
+    {"from": "maria", "to": "ored95", "amount": 200},
+    {"from": "ored95", "to": "kate", "amount": 300}])
 
 # test a simple blockchain
 blockchain = ORDchain("ored95")
 print(blockchain)
-blockchain.add_block("Binh Nguyen")
-blockchain.add_block("Alex Nguyen")
-blockchain.add_block("BMSTU iu7")
+blockchain.add_block(transaction_03_31.details)
+blockchain.add_block(transaction_04_01.details)
 blockchain.disp()
 
-"""
-A hacker is trying to hack our blockchain system.
-And somehow he knew our secret rule. Then, he attempted
-to modify our blockchain data in order to pass the validator.
-This is what he did.
-"""
-blockchain.chain[1].data = "Binh"
-for i in range(1, len(blockchain.chain)):
-    blockchain.chain[i].prev_hash = blockchain.chain[i-1].hash
-    blockchain.chain[i].hash = hash(blockchain.chain[i])
-    blockchain.chain[i].nonce = 0
-    while not blockchain.chain[i].hash.startswith('ffff'):
-        blockchain.chain[i].nonce += 1
-        blockchain.chain[i].hash = hash(blockchain.chain[i])
-
-print(f'\nHacked? {blockchain.is_valid()}')
-print('-------------------------------------------------')
-blockchain.disp()
+wallet_addr = "ored95"
+print(f'\nWallet address [{wallet_addr}]\'s balance: {blockchain.get_balance(wallet_addr)} ORD')
