48193c815271bd38927df864fb8499b62fb3e249
Binh D. Nguyen <stepup.ored@gmail.com>
2023-03-31 01:33:24 +0300

    update block hash with new datatype

--- block/hash.py
+++ block/hash.py
@@ -5,7 +5,7 @@ def hash(block):
     """_summary_
         Hash a block using SHA256
     """
-    data = block.data + block.prev_hash + str(block.nonce)
+    data = json.dumps(block.data) + block.prev_hash + str(block.nonce)
     data = data.encode('utf-8')     # prepare to hash
     
     return sha256(data).hexdigest() # get only hashstring
