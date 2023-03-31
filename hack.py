166cae321fa713b16316e2ddb774f205f3921511
Binh D. Nguyen <stepup.ored@gmail.com>
2023-03-31 13:16:12 +0300

    transaction hacked

--- hack.py
+++ hack.py
@@ -21,3 +21,40 @@ blockchain.disp()
 
 TODO: hack new block datatype (change transaction history)
 """
+from blockchain import ORDchain
+from block.hash import hash
+from transaction.transaction import Transaction
+
+# demo transaction (assumming that all transactions are valid)
+transaction_03_31 = Transaction([
+    {"from": "jame", "to": "ored95", "amount": 1000},
+    {"from": "ored95", "to": "alex", "amount": 100},
+    {"from": "ored95", "to": "john wick", "amount": 500}])
+
+transaction_04_01 = Transaction([
+    {"from": "maria", "to": "ored95", "amount": 200},
+    {"from": "ored95", "to": "kate", "amount": 300}])
+
+# test a simple blockchain
+wallet_addr = 'ored95'
+blockchain = ORDchain(wallet_addr)
+print(blockchain)
+blockchain.add_block(transaction_03_31.details)
+blockchain.add_block(transaction_04_01.details)
+blockchain.disp()
+print(f'\nBalance of owner [{blockchain.owner}]: {blockchain.get_balance(blockchain.owner)} ORD')
+
+blockchain.chain[1].data[0]["amount"] = 2000
+for i in range(1, len(blockchain.chain)):
+    blockchain.chain[i].prev_hash = blockchain.chain[i-1].hash
+    blockchain.chain[i].nonce = 0
+    blockchain.chain[i].hash = hash(blockchain.chain[i])
+    while not blockchain.chain[i].hash.startswith('ffff'):
+        blockchain.chain[i].nonce += 1
+        blockchain.chain[i].hash = hash(blockchain.chain[i])
+
+print('-'*32)
+print(f'!!! Hacked status: {blockchain.is_valid()}')
+print('-'*32)
+blockchain.disp()
+print(f'\nBalance of owner [{blockchain.owner}]: {blockchain.get_balance(blockchain.owner)} ORD')
